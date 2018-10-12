ef7bfd60bd86efb0347f23324a8b8026638d396d
Yasar Kücükkaya <yasar@yk-code.de>
2018-10-13 00:59:56 +0200

    first commit

---
+++ components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <img :src="'https://robohash.org/' + person.first_name + '_' + person.last_name" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+
+@Component({})
+export default class Card extends Vue {
+  @Prop()
+  person;
+}
+</script>
+
+<style scoped>
+.card {
+  font-family: 'Segoe UI', sans-serif;
+  padding: 1rem;
+  margin: 0.25rem;
+  border: 0.25rem solid gainsboro;
+}
+</style>
